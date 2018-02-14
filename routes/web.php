0fec8fef412cadd90f715c2fc928005ab78de0b9
LiuLucy <lucy933626@gmail.com>
2018-02-14 12:19:27 +0800

    add routes login and logout

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login','LoginController@show');
+Route::post('login','LoginController@login');
+Route::get('logout','LoginController@logout');
