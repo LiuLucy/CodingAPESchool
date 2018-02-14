7bdf4bece58399bf558dd13ae4bd4c51083b138f
LiuLucy <lucy933626@gmail.com>
2018-02-14 12:05:55 +0800

    add Lucy user, use php artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('UserTableSeeder');
+    }
+}
+
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        \App\User::create([
+            'name' => 'Bo_Hong_Liou',
+            'nick_name' => 'Lucy',
+            'gender' => '1',
+            'email' => 'lucy933626@gmail.com',
+            'password' =>  Hash::make('lucy0970'),
+            'birthday' => '1995/09/16',
+            'card_id' => 'N125954298',
+        ]);
     }
 }
