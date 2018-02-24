b37d250ca314b81f80c7d44d011fe3704fd712dd
LiuLucy <lucy933626@gmail.com>
2018-02-24 13:54:50 +0800

    users auth path

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,11 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+
+        'users' => [
+            'driver' => 'token',
+            'provider' => 'users',
+        ],
     ],
 
     /*
