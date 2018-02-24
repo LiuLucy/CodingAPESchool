0d1cac829f9b5f45f8f1d9edd9482d040b350f58
LiuLucy <lucy933626@gmail.com>
2018-02-24 14:07:07 +0800

    modified view loyouts app login index page update real path

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('user.auth.login') }}">
+                    <form class="form-horizontal" method="POST" action="{{ action('User\LoginController@showLoginForm') }}">
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
